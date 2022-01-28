44eb8f6a1621a64aa85b7ecfa88b5311a4074dfd
yihuangxing <13764664731@163.com>
2022-01-28 10:56:53 +0800

    no message

---
+++ app/src/main/java/com/app/note/activity/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.app.note.activity;
+
+import com.app.note.R;
+import com.app.note.base.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+        setStatusBarDarkMode();
+
+    }
+}
